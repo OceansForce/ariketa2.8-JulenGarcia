a4ea6d160f4f365e28976b47b491841cdd4ef21c
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-14 13:53:12 +0100

    Kenketak geitzen

--- src/Main.java
+++ src/Main.java
@@ -17,34 +17,41 @@ public class Main {
         sc.close();
 
         switch (aukera) {
-            case(1):
-                float batu = batuketa(zenb1,zenb2);
-                System.out.println("Eragiketaren emaitza "+batu+" da.");
+            case (1):
+                float batu = batuketa(zenb1, zenb2);
+                System.out.println("Eragiketaren emaitza " + batu + " da.");
 
-            case(2):
-                float kenk = kenketa(zenb1,zenb2);
-                System.out.println("Eragiketaren emaitza "+kenk+" da.");
+            case (2):
+                float kenk = kenketa(zenb1, zenb2);
+                System.out.println("Eragiketaren emaitza " + kenk + " da.");
 
-            case(3):
-                float bider = biderketa(zenb1,zenb2);
-                System.out.println("Eragiketaren emaitza "+bider+" da.");
+            case (3):
+                float bider = biderketa(zenb1, zenb2);
+                System.out.println("Eragiketaren emaitza " + bider + " da.");
 
-            case(4):
-                float zati = zatiketa(zenb1,zenb2);
-                System.out.println("Eragiketaren emaitza "+zati+" da.");
+            case (4):
+                float zati = zatiketa(zenb1, zenb2);
+                System.out.println("Eragiketaren emaitza " + zati + " da.");
 
-            case(5):
-                double ber = berreketa(zenb1,zenb2);
-                System.out.println("Eragiketaren emaitza "+ber+" da.");
+            case (5):
+                double ber = berreketa(zenb1, zenb2);
+                System.out.println("Eragiketaren emaitza " + ber + " da.");
 
-            case(6):
-                float hond = hondarra(zenb1,zenb2);
-                System.out.println("Eragiketaren emaitza "+hond+" da.");
+            case (6):
+                float hond = hondarra(zenb1, zenb2);
+                System.out.println("Eragiketaren emaitza " + hond + " da.");
         }
     }
 
-    public static float batuketa(float zenbA , float zenbB) {
+    public static float batuketa(float zenbA, float zenbB) {
         float emaitza = 1;
-        emaitza= zenbA+zenbB;
+        emaitza = zenbA + zenbB;
         return emaitza;
     }
+
+    public static float kenketa(float zenbA, float zenbB) {
+        float emaitza = 1;
+        emaitza = zenbA - zenbB;
+        return emaitza;
+    }
+}
